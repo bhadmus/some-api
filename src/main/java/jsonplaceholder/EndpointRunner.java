6b88a672868c696f5864dd6ef35e0a3ea24340c9
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2022-08-18 23:32:19 +0100

    added comments

--- src/main/java/jsonplaceholder/EndpointRunner.java
+++ src/main/java/jsonplaceholder/EndpointRunner.java
@@ -16,8 +16,11 @@ public class EndpointRunner {
     Random picker = new Random();
     int pick = getRandomUserID(getAllUserID());
 
-    // Get all userIDs
+
     public ArrayList getAllUserID(){
+        /**
+            Get all userIDs from the all users endpoint
+         **/
         Response resp = given()
                 .contentType(ContentType.JSON)
                 .when().get(baseURI+"/users");
@@ -25,19 +28,30 @@ public class EndpointRunner {
         return id;
     }
 
-    // Gets a random userID
+
     public int getRandomUserID(ArrayList m){
+        /**
+          *  Gets a random userID from any Arraytlist
+         **/
         return (int) m.get(picker.nextInt(m.size()));
     }
 
+
     public boolean rangeChecker(ArrayList l){
+        /**
+         * Checks if a list of number is withing 1 and 100
+         */
         for(int i=0; i < l.size(); i++){
             return((int)l.get(i)>1  && (int)l.get(i)<100);
         }
         return false;
     }
 
+
     public void logRandomUserEmailToConsole(){
+        /**
+         * This would log a user email based on any random ID parsed into the baseUrl
+         */
         String basePATH = String.format(baseURI+"/users/%d", pick);
         Response resp = given()
                 .contentType(ContentType.JSON)
@@ -47,7 +61,11 @@ public class EndpointRunner {
         System.out.println(email);
     }
 
+
     public void getIDsAndVerifyRange(){
+        /**
+         * Gets all userID of a random ID and checks if they are between 1 to 100
+         */
         String basePATH = String.format(baseURI+"/users/%d/posts", pick);
         Response resp = given()
                 .contentType(ContentType.JSON)
@@ -58,6 +76,10 @@ public class EndpointRunner {
     }
 
     public void postCommentWithRandomID(){
+        /**
+         * Posts a comment based on a random ID with non-empty title and body
+         * and verify the response
+         */
         Response resp = given()
                 .contentType(ContentType.JSON)
                 .body(String.format("{\n" +
